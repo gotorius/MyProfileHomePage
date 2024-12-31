0fd223367474080ed01d4342db79e5e062f6d41d
gotorius <ryosuke1108g@yahoo.ne.jp>
2024-12-31 17:15:58 +0900

    20241231

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+document.getElementById('change-message').addEventListener('click', () => {
+    const greeting = document.getElementById('greeting');
+    greeting.textContent = '新しいメッセージへようこそ！';
+  });
+  
